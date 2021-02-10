b90afdae2bdc53c4e41e558fcf1c7a06fe7584d5
kpeakers <60412112+kpeakers@users.noreply.github.com>
2021-02-10 13:09:40 -0700

    initial commit

---
+++ js/content.js
@@ -0,0 +1,20 @@
+console.log("extension is running!")
+
+var imgURL = chrome.runtime.getURL("../images/wow.gif");
+
+$("header").after("<img src=" + imgURL + " class='wow'>");
+console.log("wow?");
+
+$("p").after("yoyo from yo extension")
+
+$(".hero-header .brand-header span").text("YO WTF")
+
+
+$("main").after("<img src='images/wow.gif'>");
+
+
+// TRY: updating the text content of an element or set of elements on one or more pages
+
+// TRY: adding a click event listener to an element that changes the background of that element to a new color
+
+// TRY: using a mouseenter event listener to update text to something new.
