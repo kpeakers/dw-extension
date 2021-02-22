eddbbfcebd1cf458db7734bf48ace8a8ee581e18
kpeakers <60412112+kpeakers@users.noreply.github.com>
2021-02-21 18:18:28 -0700

    added more words and working with sounds now

--- js/content.js
+++ js/content.js
@@ -11,15 +11,35 @@ var stickers = [chrome.runtime.getURL("../images/lotr1.gif"),
                 chrome.runtime.getURL("../images/skyrim3.gif"),
                 chrome.runtime.getURL("../images/skyrim4.gif")];
 
+    
+//var backgrounds = [dusk, forest, moss, horizon];
+
+// ^^ How to access content
+
+function playRandomSound(){
+
+      //An array to house all of the URLs of your sounds
+   
 var sounds = [chrome.runtime.getURL("../sounds/hptheme.mp3"),
               chrome.runtime.getURL("../sounds/kneearrow.mp3"),
               chrome.runtime.getURL("../sounds/shallnotpass.mp3"),
               chrome.runtime.getURL("../sounds/skyrimdrum.mp3"),
               chrome.runtime.getURL("../sounds/skyrimlvlup.mp3")];
-    
-//var backgrounds = [dusk, forest, moss, horizon];
+      
+      //This line will select a random sound to play out of your provided URLS
+      var soundFile = sounds[Math.floor(Math.random()*sounds.length)];
+      
+      //Find the player element that you created and generate an embed file to play the sound within it
+      document.getElementById("player").innerHTML="<embed src=\""+soundFile+"\" hidden=\"true\" autostart=\"true\" loop=\"false\" />";
+}
 
-// ^^ How to access content
+// ^^ https://forums.asp.net/t/1876176.aspx?Making+a+random+sound+play+when+a+button+is+clicked
+
+$("body").on("click", function playRandomSound(){
+    
+    console.log("PLAYING");
+    
+});
 
 
 
@@ -52,14 +72,14 @@ function findAndReplace(){
 
 function replaceText (node) {
   let value = node.nodeValue;
-  value = value.replace(/Your/gi, "Ye ole'");
-  value = value.replace(/your/gi, "ye ole'");
+  value = value.replace(/Your /gi, "Ye ole' ");
+  value = value.replace(/your /gi, "ye ole' ");
     
   value = value.replace(/You're/gi, "Ye be");
   value = value.replace(/you're/gi, "ye be");
     
-  value = value.replace(/The/gi, "Thee");
-  value = value.replace(/the/gi, "thee");
+  value = value.replace(/The /gi, "Thee ");
+  value = value.replace(/the /gi, "thee ");
     
   value = value.replace(/Error 404/gi, "YOU SHALL NOT PASS");
   value = value.replace(/error 404/gi, "YOU SHALL NOT PASS");
@@ -67,8 +87,8 @@ function replaceText (node) {
   value = value.replace(/Trump/gi, "Sauron");
   value = value.replace(/TRUMP/gi, "SAURON");
     
-  value = value.replace(/But/gi, "Alas");
-  value = value.replace(/but/gi, "alas");
+  value = value.replace(/But /gi, "Alas ");
+  value = value.replace(/but /gi, "alas ");
     
   value = value.replace(/Yes/gi, "Aye");
   value = value.replace(/yes/gi, "aye");  
@@ -76,23 +96,21 @@ function replaceText (node) {
   value = value.replace(/No /gi, "Nay ");
   value = value.replace(/no /gi, "nay "); 
     
-  value = value.replace(/Here/gi, "Hither");
-  value = value.replace(/here/gi, "hither"); 
+  value = value.replace(/Here /gi, "Hither ");
+  value = value.replace(/here /gi, "hither "); 
     
   value = value.replace(/Covid-19/gi, "The Plague");
   value = value.replace(/COVID-19/gi, "THE PLAGUE"); 
   value = value.replace(/covid-19/gi, "the plague");
     
-  value = value.replace(/ I /gi, "Thine ");
+  value = value.replace(/I /gi, "Thine ");
     
   value = value.replace(/You /gi, "Ye ");
   value = value.replace(/you /gi, "ye "); 
     
-
-    
-    
-    
-    
+  value = value.replace(/My /gi, "Mine ");
+  value = value.replace(/my /gi, "mine "); 
+     
     
   node.nodeValue = value;
 }
@@ -101,11 +119,3 @@ window.onload = findAndReplace();
 
 //  ^^ CODE FROM https://nkhilv.medium.com/how-to-make-a-word-replacement-chrome-extension-17cbd3639db6
 
-
-
-
-// TRY: updating the text content of an element or set of elements on one or more pages
-
-// TRY: adding a click event listener to an element that changes the background of that element to a new color
-
-// TRY: using a mouseenter event listener to update text to something new.
